c1694e2280db3ca36151c409e281aaac59a8a008
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-02-09 15:40:56 -0800

    worked on the layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import Forgot from './pages/auth/Forgot'
 import Reset from './pages/auth/Reset'
+import Dashboard from './pages/dashboard/Dashboard'
+import Sidebar from './components/sidebar/Sidebar'
+import Layout from './components/layout/Layout'
 function App() {
   return (
       <BrowserRouter>
@@ -13,6 +16,11 @@ function App() {
          <Route path='/login' element={<Login/>}/>
          <Route path='/forgot' element={<Forgot/>}/>
          <Route path='/resetpassword/:resetToken' element={<Reset/>}/>
+          <Route path='/dashboard' element={ <Sidebar>
+            <Layout>
+              <Dashboard/>
+            </Layout>
+           </Sidebar>}/>
         </Routes>
       </BrowserRouter>
   )
