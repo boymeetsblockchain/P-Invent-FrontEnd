7d7573b558be1819ef2f836227f723a25c078f35
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-02-15 21:57:29 -0800

    worked on some bug

--- src/pages/profile/EditProfile.jsx
+++ src/pages/profile/EditProfile.jsx
@@ -54,12 +54,12 @@ const EditProfile = () => {
       ) {
         const image = new FormData();
         image.append("file", profileImage);
-        image.append("cloud_name", "zinotrust");
-        image.append("upload_preset", "wk66xdkq");
+        image.append("cloud_name", "dxxb653am");
+        image.append("upload_preset", "vee0nodo");
 
         // First save image to cloudinary
         const response = await fetch(
-          "https://api.cloudinary.com/v1_1/zinotrust/image/upload",
+          "https://api.cloudinary.com/v1_1/dxxb653am/image/upload",
           { method: "post", body: image }
         );
         const imgData = await response.json();
